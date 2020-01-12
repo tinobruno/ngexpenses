860f6996181bcf958ebb4436c9436558edf0fd6d
agatibr <Agatino-fabio.bruno@ext.ec.europa.eu>
2020-01-12 22:02:23 +0100

    added ngmaterial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ExpenseRow } from './components/expenserow'
 import {environment} from "../environments/environment";
+import {AngularFireDatabase} from "@angular/fire/database";
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AngularFireModule.initializeApp(environment.firebase),
-  AngularFireStorageModule],
+  AngularFireStorageModule,AngularFireDatabase],
   declarations: [ AppComponent, HelloComponent, ExpenseRow ],
   bootstrap:    [ AppComponent ]
 })
